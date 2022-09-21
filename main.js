a86d77bd7547b76b88c28fc0d1142f8499f11624
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2022-09-21 15:07:29 -0400

    Enable weather search by city & display weather

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 const container = document.querySelector(".container");
+const card = document.querySelector('.card');
+const cityName = document.querySelector(".cityName");
+const temperature = document.querySelector('.temperature');
+const feelsLike = document.querySelector('.feelsLike');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
 const form = document.querySelector(".form");
 const search = document.getElementById("search");
 
-async function SearchWeatherByCity() {
+form.addEventListener("submit", SearchWeatherByCity);
+form.addEventListener("submit", (e) => {
+  card.style.border = "1px solid black";
+  e.preventDefault();
+});
 
-};
+async function SearchWeatherByCity() {
+  let searchValue = search.value; // Create a variable with the input value
+  if (!searchValue) {
+    searchValue = "Gainesville"; // If the value is empty, we put the 'random' value
+  }
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=327aafdf4dda20dbcc0268009e351b2e&units=imperial`,
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+  cityName.innerText = weatherData.name;
+  temperature.innerText = weatherData.main.temp + " °F";
+  feelsLike.innerText = "Feels like: " + weatherData.main.feels_like + " °F";
+  humidity.innerText = "Humidity: " + weatherData.main.humidity + " %";
+  wind.innerText = "Wind: " + weatherData.wind.speed + " mph"
+}
